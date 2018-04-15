4c5e7cda0b57b1d671387bf936af26ce024a74ab
chainet <chainet@gmail.com>
2018-04-15 10:01:03 +0800

    图形编程

--- 注释.py
+++ 注释.py
@@ -1,4 +1,3 @@
-import easygui
 
 # f = easygui.buttonbox('Do you want to play this game?',
 #                      choices = ['yes','no'])
@@ -6,4 +5,8 @@ import easygui
 #asdfjkl;
 '''这是一行注释'''
 print('hello')
+#天啊
 
+'''看不见了耶'''#你看不见我
+print('I love you.   ' '''在print里面也看不见！哇咔咔咔咔！！！只能看见一个我爱你！''')
+print('真的吗??? 【奸笑】')
